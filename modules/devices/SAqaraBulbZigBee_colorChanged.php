6668a8da2ff97427ed81a7b1ede30b97ab4615dc
daniral <daniralgal@gmail.com>
2024-11-23 19:30:19 +0200

    5

--- modules/devices/SAqaraBulbZigBee_colorChanged.php
+++ modules/devices/SAqaraBulbZigBee_colorChanged.php
@@ -2,6 +2,6 @@
 
 if ($params['NEW_VALUE'] == $params['OLD_VALUE']) return;
 
-if ($params['NEW_VALUE'] >= $this->getProperty('color_tempMin') && $params['NEW_VALUE'] <= $this->getProperty('color_tempMax')) {
+if ($params['NEW_VALUE'] >= $this->getProperty('color_tempMin') && $params['NEW_VALUE'] <= $this->getProperty('color_tempMax') && $this->getProperty('flag')) {
     $this->setProperty('color_seved', $params['NEW_VALUE']);
 }
